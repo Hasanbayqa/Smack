ec8dfc0d678d491740ee37d96a5b3ad97dc7bf93
Hasanbayqa <hasanshirmatov@yahoo.com>
2018-07-19 23:28:37 -0700

    End of Lesson 5.

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,22 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Khasan Shirmatov on 7/19/18.
+//  Copyright © 2018 Khasan Shirmatov. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+}
