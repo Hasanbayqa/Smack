710dee40b08d667705ff50efcd24cf413b842a3f
Hasanbayqa <hasanshirmatov@yahoo.com>
2018-07-23 00:03:48 -0700

    End of Lesson 11

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -14,6 +14,7 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 //URL Constants
 let BASE_URL = "https://chayhanachat.herokuapp.com/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 //Segues
 
@@ -25,3 +26,9 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+
+let HEADER = [
+    "Content-Type": "application/json=utf-8"
+]
